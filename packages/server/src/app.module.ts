8699e5c40c9ee3e117adf02a91659d93408092ec
Kuro091 <lcminhhp@gmail.com>
2023-09-05 09:52:34 +0700

    Generated NestJs Backend - NestJS - Prisma - GraphQL

---
+++ packages/server/src/app.module.ts
@@ -0,0 +1,43 @@
+import { GraphQLModule } from '@nestjs/graphql';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AppResolver } from './app.resolver';
+import config from './common/configs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GqlConfigService } from './gql-config.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          // configure your prisma middleware
+          loggingMiddleware({
+            logger: new Logger('PrismaMiddleware'),
+            logLevel: 'log',
+          }),
+        ],
+      },
+    }),
+
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GqlConfigService,
+    }),
+
+    AuthModule,
+    UsersModule,
+    PostsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
---
+++ packages/server/src/modules/posts/models/post-connection.model.ts
@@ -0,0 +1,6 @@
+import { ObjectType } from '@nestjs/graphql';
+import { Post } from './post.model';
+import PaginatedResponse from '@/common/pagination/pagination';
+
+@ObjectType()
+export class PostConnection extends PaginatedResponse(Post) {}
