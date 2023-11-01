028d80d7f7322fc8f47c33c6308ed41acd81a0dd
Kuro091 <lcminhhp@gmail.com>
2023-11-01 10:14:13 +0700

    add calendars test endpoint

--- packages/server/src/modules/auth/auth.resolver.ts
+++ packages/server/src/modules/auth/auth.resolver.ts
@@ -28,8 +28,12 @@ export class AuthResolver {
   @Query(() => GraphQLJSON, { name: 'me' })
   @UseGuards(AzureAuthGuard)
   async login(@UserEntity() user: any) {
-    const test1 = await this.graphService.getUser(user.oid);
-    const test2 = await this.graphService.getUserCalendars(user.oid);
     return user;
+  }
+
+  @Query(() => GraphQLJSON, { name: 'getSelfCalendars' })
+  @UseGuards(AzureAuthGuard)
+  async getSelfCalendars(@UserEntity() user: any) {
+    return await this.graphService.getUserCalendars(user.oid);
   }
 }
