f880c8ff61af16731b8b2520059be633246d7a1e
phu phan <>
2023-11-05 17:17:53 +0700

    fixbug

--- packages/app/src/screens/plan/SearchPlanScreen.tsx
+++ packages/app/src/screens/plan/SearchPlanScreen.tsx
@@ -2,15 +2,22 @@ import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import Container from 'elements/Layout/Container';
 import HeaderPlan from 'screens/plan/components/HeaderPlan';
+import scale from 'utils/scale';
 
 interface SearchPlanScreenProps {}
 
 const SearchPlanScreen = (props: SearchPlanScreenProps) => {
   const [state, setState] = useState();
+
+  const onFilter = () => {};
+  const onSearch = () => {};
+
   return (
     <Container hideHeader={true} style={styles.container}>
-      <HeaderPlan />
-      <Text>SearchPlanScreen</Text>
+      <HeaderPlan onFilter={onFilter} onSearch={onSearch} />
+      <View style={styles.bodyContainer}>
+        <Text style={styles.title}>Recents</Text>
+      </View>
     </Container>
   );
 };
@@ -19,4 +26,11 @@ export default SearchPlanScreen;
 
 const styles = StyleSheet.create({
   container: {},
+  bodyContainer: {
+    padding: scale(16),
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
--- packages/app/src/screens/plan/components/HeaderPlan.tsx
+++ packages/app/src/screens/plan/components/HeaderPlan.tsx
@@ -7,6 +7,7 @@ import {
   ImageSourcePropType,
   StyleProp,
   ViewStyle,
+  TouchableOpacity,
 } from 'react-native';
 import ButtonIcon from 'elements/Buttons/ButtonIcon';
 import images from 'res/images';
@@ -31,17 +32,31 @@ import useStateCustom from 'hooks/useStateCustom';
 interface HeaderPlanProps {
   title?: string;
   onSearch: (text?: string) => void;
+  onFilter: (text: string) => void;
 }
 
 interface IState {
   isShowSearch?: boolean;
   keyword?: string;
+  filter: 'All' | 'Cases' | 'Calls';
 }
-const HeaderPlan = ({title, onSearch}: HeaderPlanProps) => {
+const HeaderPlan = ({title, onSearch, onFilter}: HeaderPlanProps) => {
   const [state, setState] = useStateCustom<IState>({
     isShowSearch: false,
     keyword: '',
+    filter: 'All',
   });
+  const dataFilter = [
+    {
+      title: 'All',
+    },
+    {
+      title: 'Cases',
+    },
+    {
+      title: 'Calls',
+    },
+  ];
   const router = useNavigation();
   const anim = useSharedValue(0);
   const styleAnim = useAnimatedStyle(() => ({
@@ -72,36 +87,62 @@ const HeaderPlan = ({title, onSearch}: HeaderPlanProps) => {
   };
   const onPressBack = () => router.goBack();
   const renderSearch = () => (
-    <View style={styles.groupHeader}>
-      <Animated.View style={[{flex: 1, marginRight: 10}]}>
-        <TextInput
-          placeholder={'Search'}
-          backgroundColor={colors.white}
-          style={{borderRadius: 50, height: 30, paddingLeft: 5}}
-          editable={true}
-          autoFocus={true}
-          value={state.keyword || ''}
-          onChangeText={onChangeText}
-          iconLeft={<Image source={images.ic_search} />}
-          isShowIconLeft={true}
+    <>
+      <View style={styles.groupHeader}>
+        <Animated.View style={[{flex: 1, marginRight: 10}]}>
+          <TextInput
+            placeholder={'Search'}
+            backgroundColor={colors.white}
+            style={{borderRadius: 50, height: 30, paddingLeft: 5}}
+            editable={true}
+            autoFocus={true}
+            value={state.keyword || ''}
+            onChangeText={onChangeText}
+            iconLeft={<Image source={images.ic_search} />}
+            isShowIconLeft={true}
+          />
+        </Animated.View>
+        <ButtonText
+          title={strings.cancel}
+          titleColor={colors.white}
+          textProps={{
+            fontWeight: '600',
+            size: 17,
+          }}
+          onPress={() => {
+            if (state.keyword) {
+              setState({isShowSearch: false, keyword: ''});
+            } else {
+              onPressBack();
+            }
+          }}
         />
-      </Animated.View>
-      <ButtonText
-        title={strings.cancel}
-        titleColor={colors.white}
-        textProps={{
-          fontWeight: '600',
-          size: 17,
-        }}
-        onPress={() => {
-          if (state.keyword) {
-            setState({isShowSearch: false, keyword: ''});
-          } else {
-            onPressBack();
-          }
-        }}
-      />
-    </View>
+      </View>
+      {onFilter && (
+        <View style={styles.wrapButtonContainer}>
+          {dataFilter.map(item => {
+            return (
+              <TouchableOpacity
+                style={
+                  item.title == state.filter
+                    ? styles.wrapButtonChoose
+                    : styles.wrapButton
+                }
+                onPress={() => setState({filter: item.title})}>
+                <Text
+                  style={
+                    item.title == state.filter
+                      ? styles.wrapButtonTextChoose
+                      : styles.wrapButtonText
+                  }>
+                  {item.title}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      )}
+    </>
   );
   return <SafeAreaView style={styles.container}>{renderSearch()}</SafeAreaView>;
 };
@@ -138,11 +179,36 @@ const styles = StyleSheet.create({
   groupHeader: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingVertical: scale(15),
     paddingHorizontal: scale(16),
+    marginTop: scale(16),
+    marginBottom: scale(4),
   },
   container: {
     backgroundColor: colors.primary,
     paddingTop: sizes._statusbar_height + 15,
+  },
+  wrapButtonContainer: {
+    marginLeft: scale(16),
+    marginBottom: scale(8),
+    flexDirection: 'row',
+  },
+  wrapButton: {
+    paddingVertical: scale(4),
+    paddingHorizontal: scale(16),
+    backgroundColor: colors.white,
+    marginRight: scale(8),
+    borderRadius: scale(16),
+  },
+  wrapButtonChoose: {
+    paddingVertical: scale(4),
+    paddingHorizontal: scale(16),
+    backgroundColor: colors.blue3,
+    marginRight: scale(8),
+    borderRadius: scale(16),
+  },
+  wrapButtonText: {},
+  wrapButtonTextChoose: {
+    color: colors.white,
+    fontWeight: '600',
   },
 });
