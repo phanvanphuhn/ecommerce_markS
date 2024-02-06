5752d2e6be85ae3220bed4e778c96df38afdcdc8
Kuro091 <lcminhhp@gmail.com>
2024-02-06 13:06:36 +0700

    change how we encode db passwords

--- packages/server/entrypoint.sh
+++ packages/server/entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
 encodeReservedCharacters() {
-  local inputString="$1"
-  local charactersToEncode='[?:\\/&=+<>]'
+  # local inputString="$1"
+  # local charactersToEncode='[?:\\/&=+<>]'
 
-  echo "$inputString" | sed -E "s/$charactersToEncode/$(printf '%%%02X\n' "'\0")/g"
+  # echo "$inputString" | sed -E "s/$charactersToEncode/$(printf '%%%02X\n' "'\0")/g"
+  local input_string=$1
+  local characters_to_encode='[?:\\/&=+<>]'
+  local match
+  local encoded_string
+  
+  for ((i = 0; i < ${#input_string}; i++)); do
+    match=${input_string:i:1}
+    if [[ $match =~ $characters_to_encode ]]; then
+      encoded_string+='%'
+      encoded_string+=$(printf '%02X' "'$match")
+    else
+      encoded_string+=$match
+    fi
+  done
+  echo "$encoded_string"
 }
 
 set -e
@@ -15,7 +30,7 @@ export $(cat $SECRETS_FILE_PATH | jq -r 'to_entries | .[] | .key + "=" + (.value
 # rename host to DB_HOST, username to POSTGRES_USER, password to POSTGRES_PASSWORD, port to DB_PORT
 export DB_HOST=$host
 export POSTGRES_USER=$username
-export POSTGRES_PASSWORD='E^*y,p`*gE2M*v%3E|Gpu5%3F7nDIOi0)%3E|]'
+export POSTGRES_PASSWORD=$password
 export DB_PORT=$port
 export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}:${DB_PORT}/${POSTGRES_DB}?schema=${DB_SCHEMA}&sslmode=prefer"
 
