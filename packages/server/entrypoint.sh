3e96258d1143f7b040d129c388c150613642de9a
Kuro091 <lcminhhp@gmail.com>
2024-02-06 09:14:01 +0700

    temp fix for password

--- packages/server/entrypoint.sh
+++ packages/server/entrypoint.sh
@@ -15,7 +15,7 @@ export $(cat $SECRETS_FILE_PATH | jq -r 'to_entries | .[] | .key + "=" + (.value
 # rename host to DB_HOST, username to POSTGRES_USER, password to POSTGRES_PASSWORD, port to DB_PORT
 export DB_HOST=$host
 export POSTGRES_USER=$username
-export POSTGRES_PASSWORD="E^*y,p`*gE2M*v%3E|Gpu5%3F7nDIOi0)%3E|]"
+export POSTGRES_PASSWORD='E^*y,p`*gE2M*v%3E|Gpu5%3F7nDIOi0)%3E|]'
 export DB_PORT=$port
 export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}:${DB_PORT}/${POSTGRES_DB}?schema=${DB_SCHEMA}&sslmode=prefer"
 
